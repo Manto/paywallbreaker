86a2b7b88a2e27a29847159ccd06990a54d971cb
Shawn Chiao <mantle@gmail.com>
2020-05-13 14:51:22 -0700

    fixes bloomberg paylwall break

--- popup.js
+++ popup.js
@@ -4,36 +4,48 @@ breakwall.onclick = function (element) {
       code: `
 
 // For Bloomberg //
-els = document.getElementsByClassName("main-column-v2")
-if (els.length > 0) {
-  el = els[0]
-  blocker = el.getElementsByClassName("paywall-inline-tout")[0]
-  blocker.style.display = "none";
-
-  paragraphs = el.getElementsByTagName("p")
-  for(let i=0; i < paragraphs.length; i++) {
-    if (paragraphs[i].style.display === "none") {
-      paragraphs[i].style.display = "";
-    }
-  }
-
-  tables = el.getElementsByTagName("p")
-  for(let i=0; i < tables.length; i++) {
-    if (tables[i].style.display === "none") {
-      tables[i].style.display = "";
-    }
-  }
+el = document.getElementById('graphics-paywall-overlay')
+if (el) {
+  el.style.display = "none";
 }
 
-// For Economist //
-articles = document.getElementsByTagName("article");
-if (articles.length > 0) {
-  article = articles[0]
-  chrome.storage.local.get(window.location.href, function(result) {
-    console.log(result[window.location.href])
-    article.innerHTML = result[window.location.href]
-  });
-}
+b = document.getElementsByTagName("body")[0]
+b.setAttribute('data-paywall-overlay-status', '')
+b.style.overflow = '';
+
+
+// els = document.getElementsByClassName("main-column-v2")
+// if (els.length > 0) {
+//   el = els[0]
+//   blocker = el.getElementsByClassName("paywall-inline-tout")[0]
+//   blocker.style.display = "none";
+
+//   paragraphs = el.getElementsByTagName("p")
+//   for(let i=0; i < paragraphs.length; i++) {
+//     if (paragraphs[i].style.display === "none") {
+//       paragraphs[i].style.display = "";
+//     }
+//   }
+
+//   tables = el.getElementsByTagName("p")
+//   for(let i=0; i < tables.length; i++) {
+//     if (tables[i].style.display === "none") {
+//       tables[i].style.display = "";
+//     }
+//   }
+// }
+
+
+// For Economist & Bloomberg //
+chrome.storage.local.get(window.location.href, function(result) {
+  if (result[window.location.href]) {
+    articles = document.getElementsByTagName("article");
+    if (articles.length > 0) {
+      article = articles[0]    
+      article.innerHTML = result[window.location.href]
+    }
+  }
+});
 
 // els = document.getElementsByClassName("paywall")
 // if (els.length > 0) {
